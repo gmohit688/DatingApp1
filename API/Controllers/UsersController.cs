62818b93bfbd8d0dc30f7e62abd86ed6e82d4d83
gmohit688@gmail.com <gmohit688@gmail.com>
2022-11-14 17:19:38 +0530

    show user info on web app with angular

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -19,15 +20,13 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<AppUser>> GetUsers(){
-            var users = _context.Users.ToList();
-
-            return users;
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+            return await _context.Users.ToListAsync();
         }
 
         [HttpGet("{id}")]
-        public ActionResult<AppUser> GetUser(int id){
-            return _context.Users.Find(id);
+        public async Task<ActionResult<AppUser>> GetUser(int id){
+            return await _context.Users.FindAsync(id);
         }
 
     }
